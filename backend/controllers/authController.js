e8226b27d4bfad6cce7afab586a5d6c109c39883
aniketh <sunnyrockzzz1811@gmail.com>
2025-01-08 08:32:42 +0530

    Add signup api

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,9 +1,10 @@
 import bcrypt from "bcrypt"
+import { createNewUser } from "../db/queries.js"
 
 export async function signup(req, res){
     const username = req.body.username;
     const password = await bcrypt.hash(req.body.password, 10);
     
-    console.log(username + " " + password)
+    await createNewUser(username, password)
     res.redirect("/login")
 }
