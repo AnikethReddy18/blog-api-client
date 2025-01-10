10de338b21f8e1d426043ae847107cc23ee5f772
aniketh <sunnyrockzzz1811@gmail.com>
2025-01-10 08:09:18 +0530

    Imporve UI

--- view-blogs/src/App.jsx
+++ view-blogs/src/App.jsx
@@ -4,24 +4,25 @@ import Post from './components/post'
 
 function App() {
   const [posts, setPosts] = useState(null)
-  
-  async function getPosts(){
+
+  async function getPosts() {
     const response = await fetch("http://localhost:3000/posts")
     const data = await response.json()
     setPosts(data)
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     getPosts()
   }, [])
 
   return (
     <>
       {posts ? (
-        <ul>
-          {posts.map((post, index)=><Post title={post.title} content={post.content} date={post.date} key={index}/>)}
-        </ul>
-      ): "loading"}
+        <div className="posts">
+         { posts.map((post, index) =>
+          <Post title={post.title} content={post.content} date={post.date} key={index} />)}
+        </div>
+      ) : "Loading Posts"}
     </>
   )
 }
