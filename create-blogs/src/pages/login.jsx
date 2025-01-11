6c7a1a12a360af77797ae088a624afe1de40bde1
aniketh <sunnyrockzzz1811@gmail.com>
2025-01-11 08:29:22 +0530

    Add Login

--- create-blogs/src/pages/login.jsx
+++ create-blogs/src/pages/login.jsx
@@ -1,10 +1,40 @@
+import { useState } from "react";
+import axios from "axios"
+import { useNavigate } from "react-router-dom";
+
 function Login() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate()
+
+    async function login(e){
+        e.preventDefault();
+        try{
+            const response =  await axios.post("http://localhost:3000/login", 
+                { username, password },
+                {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+            );
+
+            const token = response.data.token
+            localStorage.setItem("token", token);
+            navigate("/")
+        }catch(err){
+            console.log(err)
+        }
+
+    }
+
     return (
-        <form action="/login">
-            <input name="username" type="text"/>
-            <input name="password" type="password"/>
-            <button>Log in</button>
-        </form>);
+    <form onSubmit={login}>
+        <input name="username" type="text" onChange={(e)=>setUsername(e.target.value)}/>
+        <input name="password" type="password"  onChange={(e)=>setPassword(e.target.value)}/>
+        <button>login</button>
+    </form>);
 }
 
 export default Login;
--- create-blogs/src/pages/signup.jsx
+++ create-blogs/src/pages/signup.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
+import axios from "axios"
+import { useNavigate } from "react-router-dom";
+
 function Signup() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate()
+    async function signup(e){
+        e.preventDefault();
+        try{
+             await axios.post("http://localhost:3000/signup", 
+                { username, password },
+                {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+            );
+            navigate("/login")
+        }catch(err){
+            console.log(err)
+            window.location.reload()
+        }
+
+    }
+
     return (
-    <form action="/signup">
-        <input name="username" type="text"/>
-        <input name="password" type="password"/>
+    <form onSubmit={signup}>
+        <input name="username" type="text" onChange={(e)=>setUsername(e.target.value)}/>
+        <input name="password" type="password"  onChange={(e)=>setPassword(e.target.value)}/>
         <button>Signup</button>
     </form>);
 }
