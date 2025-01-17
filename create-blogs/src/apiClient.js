7b769f8e2f1c4c1c28c0b7d379962be1b0bca8b4
aniketh <sunnyrockzzz1811@gmail.com>
2025-01-17 07:47:24 +0530

    Error Check

--- create-blogs/src/apiClient.js
+++ create-blogs/src/apiClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiBaseUrl = import.meta.env.API_URL || "http://localhost:3000";
+const apiBaseUrl = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 const apiClient = axios.create({
     baseURL: apiBaseUrl,
