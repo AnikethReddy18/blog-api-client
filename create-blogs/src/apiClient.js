853db23017673b2b47695d7cfefc82493a012979
aniketh <sunnyrockzzz1811@gmail.com>
2025-01-17 08:01:16 +0530

    5

--- create-blogs/src/apiClient.js
+++ create-blogs/src/apiClient.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const apiBaseUrl = import.meta.env.VITE_API_URL || "http://localhost:3000";
+console.log(import.meta.env.VITE_API_URL || "http://localhost:3000")
 console.log(import.meta.env.VITE_API_URL)
-console.log("API Client Base URL:", apiClient.defaults.baseURL);
 const apiClient = axios.create({
     baseURL: apiBaseUrl,
 });
-console.log(import.meta.env.VITE_API_URL || "http://localhost:3000")
+console.log("API Client Base URL:", apiClient.defaults.baseURL);
 
 export default apiClient;
